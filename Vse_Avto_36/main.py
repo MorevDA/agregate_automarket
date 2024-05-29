8f4df67b4139fb7c42d4d4881eb394ed62a23742
Dmitry <morevda1983@gmail.com>
2024-05-29 22:30:14 +0300

    Скорректирован метод post_init - после инициализации класса  заполняются атрибуты original_parts и analog_parts

--- Vse_Avto_36/main.py
+++ Vse_Avto_36/main.py
@@ -10,9 +10,14 @@ class VA_Parts_Information(Parts_Information):
     brand: str = None
 
     def __post_init__(self):
+        """Метод для получения предложений по запрашиваемому парт-номеру (как оригинальных деталей,
+        так и замениелей"""
         self.config.params_for_brand_search['article'] = self.original_part_number
         self.config.params_for_full_search['article'] = self.original_part_number
         self._get_brand_name()
+        parts_content: dict = self._get_full_part_information().get('data')['rows']
+        self._get_original_parts(parts_content['request'])
+        self._get_analog_parts(parts_content['nonOriginalAnalog'])
 
     def _get_brand_name(self) -> None:
         """Метод для получения от API наименования бренда-производителя детали с искомым парт-номером"""
@@ -33,7 +38,7 @@ class VA_Parts_Information(Parts_Information):
         self.original_parts = Part(input_data[0]['brand'], input_data[0]['articleDisplay'], input_data[0]['name'], '')
         self.original_parts.suggestions = list(map(self.get_suggestion, input_data))
 
-    def _get_analog_parts(self, analog_json: list):
+    def _get_analog_parts(self, analog_json: list) -> None:
         """Метод для получения перечня возможных замен для искомого парт-номера"""
         analog_json.sort(key=lambda x: x['brand'])
         part_brand = Part(analog_json[0]['brand'], analog_json[0]['articleDisplay'], analog_json[0]['name'])
