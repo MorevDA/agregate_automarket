b4fb7b52229987cc14c297ff4fecc7baa3e433b0
Dmitry <morevda1983@gmail.com>
2024-05-31 00:57:37 +0300

    Скорректированы названия атрибутов класса Config для использования методов класса Parts_Information

--- Vse_Avto_36/va_config.py
+++ Vse_Avto_36/va_config.py
@@ -5,9 +5,9 @@ from requests.cookies import RequestsCookieJar
 class Config:
     def __init__(self, session):
         self.cookies_url: str = 'https://www.va36.ru'
-        self.base_search_url: str = 'https://www.va36.ru/api/v2/client/fast-search/'
+        self.search_url: str = 'https://www.va36.ru/api/v2/client/fast-search/'
         self.params_for_brand_search: dict = {'article': "", 'withAnalogs': '1'}
-        self.params_for_full_search = {'article': None, 'brand': None, 'withAnalogs': '1'}
+        self.params_for_search = {'article': None, 'brand': None, 'withAnalogs': '1'}
         self.headers = {'authority': 'www.va36.ru',
                         'accept': '*/*',
                         'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
