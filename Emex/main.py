86fd764fa07c475ad17e53df1180c93889b63aa9
Dmitry <morevda1983@gmail.com>
2024-06-02 23:55:07 +0300

    Скорректирован метод post_init - добавлен функционал для формирования са=писка аналогов

--- Emex/main.py
+++ Emex/main.py
@@ -10,10 +10,11 @@ class Emex_Parts_Information(Parts_Information):
     def __post_init__(self):
         self.config.params_for_search['detailNum'] = self.config.params_for_search['searchString']\
             = self.original_part_number
-        full_parts_data = self._get_full_part_information()
-        self.original_parts = self.get_original_part(full_parts_data['searchResult']['originals'][0])
+        full_parts_data = self._get_raw_parts_information()
+        self.original_parts = self.get_full_part_data(full_parts_data['searchResult']['originals'][0])
+        self.analog_parts = [self.get_full_part_data(analog) for analog in full_parts_data['searchResult']['analogs']]
 
-    def get_original_part(self, part_ifo: dict) -> Part:
+    def get_full_part_data(self, part_ifo: dict) -> Part:
         """Метод для получения информации по запчасти с искомым парт-номером"""
         original_part = Part(part_ifo['make'], part_ifo['detailNum'], part_ifo['name'])
         original_part.suggestions = [self.get_suggestion(offer) for offer in part_ifo['offers']]
