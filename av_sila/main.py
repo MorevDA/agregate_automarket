aad0f25e928fa6a1c5cc9d2eeaec1e62d0e7d37b
Dmitry <morevda1983@gmail.com>
2024-06-17 23:38:43 +0300

    Исправлена ошибка в методе _get_brand. Добавлен метод _get_full_parts_information для получения от API json  с полной информацией по запчасти с искомым парт-номером.

--- av_sila/main.py
+++ av_sila/main.py
@@ -9,12 +9,19 @@ class AVS_Parts_Information(Parts_Information):
     def __post_init__(self):
         self.config.params_for_search['q'] = self.original_part_number
         self._get_brand()
-        #full_parts_data = self._get_raw_parts_information()
+        # full_parts_data = self._get_raw_parts_information()
 
     def _get_brand(self):
         """Метод для получения бренда производителя детали с запрашиваемым парт-номером для дальнейшего поиска
         предложений"""
         json_data = self._get_content_post_method(url=self.config.search_url, headers=self.config.headers,
                                                   data=self.config.params_for_search)
-        self.config.params_for_search = list(json_data['catalogs'].keys())[0]
+        self.config.params_for_search['brand_title'] = list(json_data['catalogs'].keys())[0]
 
+    def _get_full_parts_information(self):
+        """Метод для получения полной информации(оригиналы и аналоги) по запчасти с искомым парт-номером"""
+        full_part_information = self._get_content_post_method(url=self.config.search_url, headers=self.config.headers,
+                                                              data=self.config.params_for_search, cookies=self.config.cookies)
+
+        return full_part_information
+    # _get_content_post_method
