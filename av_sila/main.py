9cfeaefe5bbdc0819312ed0936577b95bf2ce3e8
Dmitry <morevda1983@gmail.com>
2024-06-27 23:56:29 +0300

    Перкименован метод _get_information_original_parts - новое название _get_part_information. Так же скорректирована реализация метода - переменные brand и part_number в новой реализации получаются из словаря приходящего на вход функции.

--- av_sila/main.py
+++ av_sila/main.py
@@ -8,8 +8,8 @@ class AVS_Parts_Information(Parts_Information):
 
     def __post_init__(self):
         self.config.params_for_search['q'] = self.original_part_number
-        full_json_data = self._get_full_parts_information()['parts']
-        self.original_parts = self._get_information_original_parts(full_json_data)
+        full_json_data = self._get_full_parts_information()
+        self.original_parts = self._get_parts_information(full_json_data['analog_type_N'])
 
     def _get_full_parts_information(self) -> dict:
         """Метод для получения полной информации(оригиналы и аналоги) по запчасти с искомым парт-номером"""
@@ -17,15 +17,15 @@ class AVS_Parts_Information(Parts_Information):
                                                               data=self.config.params_for_search,
                                                               cookies=self.config.cookies)
 
-        return full_part_information
+        return full_part_information['parts']
 
-    def _get_information_original_parts(self, content: dict) -> Part:
+    def _get_parts_information(self, content: dict) -> Part:
         """Метод для формирования предложений по деталям с искомым пар-номером"""
-        brand = self.config.params_for_search['brand_title']
-        part_number = self.config.params_for_search['q']
-        suggestion_info = content['analog_type_N'][brand][part_number]['items']
-        brand_name = suggestion_info[0]['title']
-        original = Part(brand, part_number, brand_name)
+        brand = list(content)[0]
+        part_number = list(content[brand])[0]
+        suggestion_info = content[brand][part_number]['items']
+        part_name = suggestion_info[0]['title']
+        original = Part(brand, part_number, part_name)
         original.suggestions = [self._get_suggestions(supply) for supply in suggestion_info]
         return original
 
